35c392c5e8aa55d3044ab09d2757ca34c54d1965
JpMurcia <juan.murcia@udla.edu.co>
2019-11-15 23:22:02 -0500

    proyecto Cosntrupcion

---
+++ CSI/SIGEPI_CSI/WF_ext/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WF_ext.Startup))]
+namespace WF_ext
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
